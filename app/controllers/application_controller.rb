c8328ee5f5a353b5053a16f5011b409162b47276
jeromericks <jeromericks@gmail.com>
2015-12-05 13:59:14 -0600

    Added area_of_expertise to the controller and the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :first_name, :last_name,
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :first_name, :last_name, :area_of_expertise,
      :email, :password, :password_confirmation) }
   end
   # Prevent CSRF attacks by raising an exception.
